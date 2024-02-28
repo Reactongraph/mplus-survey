4cbdf29a246f85222e8413291b8c3c6412fac154
Js_developer <js.developer@ongraph.com>
2024-02-28 12:56:16 +0530

    privacy-changes

--- src/hooks/useTnc.tsx
+++ src/hooks/useTnc.tsx
@@ -15,28 +15,27 @@ const useTnc = () => {
             onClick={(e: any) => setIsFirstSelected(e.target.checked)}
           />{' '}
           <p className='text-xs'>
-            By clicking continue, I agree to the{' '}
+            I have read and agree to the uses of my
+            {/* <Link style={{ color: 'blue' }} href='/privacy-policy'> */}
+            information by Precision Research in
+            {/* </Link>{' '} */}
+            accordance with its{' '}
             <Link style={{ color: 'blue' }} href='/privacy-policy'>
               Privacy Policy
             </Link>{' '}
-            and{' '}
-            <Link style={{ color: 'blue' }} href='/terms-conditions'>
-              Terms of Use
-            </Link>{' '}
-            of Precision.
           </p>
         </div>
-        <div className='flex items-center gap-6'>
+        {/* <div className='flex items-center gap-6'>
           <input
             type='checkbox'
             checked={isSecondSelected}
             onClick={(e: any) => setIsSecondSelected(e.target.checked)}
           />{' '}
           <p className='text-xs'>I agree to receive marketing communication from SMO TEST.</p>
-        </div>
+        </div> */}
       </div>
     ),
-    isAccepted: isFirstSelected && isSecondSelected
+    isAccepted: isFirstSelected 
   };
 };
 
